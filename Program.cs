29842be6f370748a2204c0cfcdd06a7a148fbc92
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:46:22 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,64 +1,122 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace HelloWord
+namespace Esercizio_Condizioni
 {
     class Program
-
     {
+        /*
+Scrivere un programma che:
+- Chieda all'utente 2 stringhe
+- Nel caso le due stringhe siano uguali tra di loro come contenuto:
+- Stampi su schermo "Le stringhe sono uguali"
+- Nel caso la prima stringa sia più lunga della seconda:
+- Stampi su schermo "La prima stringa è più lunga della seconda"
+- Nel caso la seconda stringa sia più lunga della prima:
+- Chieda all'utente una terza stringa
+- Nel caso la terza stringa sia uguale alla prima come contenuto:
+- Stampi su schermo "Easter egg"
+*/
+
         static void Main(string[] args)
         {
+            string validStringaUno = "Admin";
+            string validStringaDue = "Admin";
+
+            Console.WriteLine("StringaUno");
+            string StringaUno = Console.ReadLine();
+
+            Console.WriteLine("StringaDue");
+            string StringaDue = Console.ReadLine();
+
+            if (StringaUno == validStringaUno && StringaDue == validStringaDue)
+
+            {
+                Console.WriteLine("Le stringe sono uguali");
+            }
+            if (StringaUno => stringaDue)
+            {
+                Console.WriteLine("La prima stringa e piu lunga della seconda");
+            }
+
+
+
 
             /*
-            string nome = "Riccardo";
-            Console.WriteLine(nome);
-            Console.WriteLine(nome);
-            Console.WriteLine(nome);
-            Console.WriteLine(nome);
-            Console.WriteLine(nome);
-            Console.WriteLine("Giulio");
-            Console.WriteLine("Riccardo");  */
-
-            //Prendere valori  inputUtente
+             * static void Main(string[] args)
+                {
 
 
-            /* Console.WriteLine("Juljan");
-
-              string valore = "Juljan";
-             Console.WriteLine(valore);
-
-              string inputUtente = Console.ReadLine();
-             Console.WriteLine(inputUtente); */
-
-
-             Console.WriteLine("Benvenuto");
-
-                 Console.WriteLine("Come ti Chiami ?");
-                 string username = Console.ReadLine();
-                    Console.WriteLine(username);
-
-                 Console.WriteLine("Che giorno è ?");
-                 string giorno = Console.ReadLine();
-                 Console.WriteLine(giorno);
-
-                 Console.ReadKey(); 
-
-           /* Console.WriteLine("Benvenuto");
-
-            Console.WriteLine("Metti un numero!");
-            string numero = Console.ReadLine();
-            Console.WriteLine("numero");
+                /*Scrivere un programma che:
+                 * chieda username e pass
+                 * valuti l'esattezza dei dati immessi rispetto a due valori preimpostati
+                 * in caso di esattezza:
+                 * dia il benvenuto al utente
+                 * in caso contrario:
+                 * mostri errore
+                 * 
             
-            Console.ReadKey(); */
+            string validUsername = "Admin";
+            string validPassword = "1234";
 
-          
+            Console.WriteLine("Username");
+            string Username = Console.ReadLine();
+
+            
+            Console.WriteLine("Password");
+            string Password = Console.ReadLine();
+
+            if (Username == validUsername && Password == validPassword)
+            {
+                Console.WriteLine("Benvenuto Admin");
+            }
+            else
+            {
+                Console.WriteLine("Errore");
+            }
+            Console.ReadKey();
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+                /*
+                 *   /* 
+                Scrivere un programma che:
+                -Prenda in input una stringa dall'utente
+                -Verifichi che la stringa comincia con la A maiuscola:
+                 - in caso positivo, chieda all'tente una seconda stringa
+                 - verifichi che la seconda stringa sia lunga 10 caratteri:
+                 - in caso positivo scriva "10" sullo schermo 
+                   */
+                /*
+                 * 
+                 * static void Main(string[] args)
+            {
+                Console.WriteLine("Inserisci una stringa qualsiasi");
+                string input = Console.ReadLine();
+                if (input.StartsWith("A"))
+                {
+                    Console.WriteLine("Inserisci una seconda stringa qualsiasi");
+                    input = Console.ReadLine();
+                    if (input.Length == 10)
+                    {
+                        Console.WriteLine("10");
+                    }
+                }
+
+
+                Console.ReadKey();
+                */
         }
     }
-
-   
-
 }
