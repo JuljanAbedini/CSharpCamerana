220bce7de5a38cea97f29e4100fcf5cbdad5c4d0
Juljan Abedini <76050221+JuljanAbedini@users.noreply.github.com>
2020-12-26 11:43:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            string nome = "Riccardo";
+            Console.WriteLine(nome);
+            Console.WriteLine(nome);
+            Console.WriteLine(nome);
+            Console.WriteLine(nome);
+            Console.WriteLine(nome);
+            Console.WriteLine("Giulio");
+            Console.WriteLine("Riccardo");  */
+
+            //Prendere valori  inputUtente
+
+
+            /* Console.WriteLine("Juljan");
+
+              string valore = "Juljan";
+             Console.WriteLine(valore);
+
+              string inputUtente = Console.ReadLine();
+             Console.WriteLine(inputUtente); */
+
+
+             Console.WriteLine("Benvenuto");
+
+                 Console.WriteLine("Come ti Chiami ?");
+                 string username = Console.ReadLine();
+                    Console.WriteLine(username);
+
+                 Console.WriteLine("Che giorno è ?");
+                 string giorno = Console.ReadLine();
+                 Console.WriteLine(giorno);
+
+                 Console.ReadKey(); 
+
+           /* Console.WriteLine("Benvenuto");
+
+            Console.WriteLine("Metti un numero!");
+            string numero = Console.ReadLine();
+            Console.WriteLine("numero");
+            
+            Console.ReadKey(); */
+
+          
+        }
+    }
+
+   
+
+}
